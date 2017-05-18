62bc43459b13ace8548813833a215c798330d3e8
Rishik <rishikbahri@gmail.com>
2017-05-18 17:11:34 +0530

    Initial Commit
    Initial commit for the app

---
+++ src/main/java/com/application/topiclish/controller/TopiclishController.java
@@ -0,0 +1,18 @@
+package com.application.topiclish.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("/")
+public class TopiclishController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView view = new ModelAndView("index");
+		return view;
+	}
+}
