654939ecded279ba0d921253dc58ebe0e7dfc41d
Rishik <rishikbahri@gmail.com>
2017-05-20 17:27:02 +0530

    Created Service layer
    Created service layer

--- src/main/java/com/application/topiclish/controller/TopiclishController.java
+++ src/main/java/com/application/topiclish/controller/TopiclishController.java
@@ -3,13 +3,24 @@ package com.application.topiclish.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.application.topiclish.service.TopiclishService;
+
 @Controller("/")
 public class TopiclishController {
+	
+	@Autowired
+	private TopiclishService topiclishService;
+	
+	private Logger log = LoggerFactory.getLogger(TopiclishController.class);
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView index(HttpServletRequest request, HttpServletResponse response){
 		ModelAndView view = new ModelAndView("index");
---
+++ src/main/java/com/application/topiclish/service/TopiclishService.java
@@ -0,0 +1,37 @@
+package com.application.topiclish.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.application.topiclish.dao.TopiclishDao;
+import com.application.topiclish.dto.Topic;
+
+@Service
+public class TopiclishService {
+
+	@Autowired
+	private TopiclishDao dataStructure;
+	
+	private Logger log = LoggerFactory.getLogger(TopiclishService.class);
+	
+	// Number of topics to be displayed
+	private final int topTopicsToRetirieve = 20;
+	
+	public void createTopic(String topicName, String topicDesc){
+		Topic topic = new Topic(topicName,topicDesc);
+		dataStructure.createTopic(topic);
+	}
+	public List<Topic> getTopTopics(){
+		return dataStructure.getTopNTopics(topTopicsToRetirieve);
+	}
+	public void upvoteTopic(String topicId, String voteCount){
+		dataStructure.upvoteTopic(topicId);
+	}
+	public void downvote(String topicId, String voteCount){
+		dataStructure.downvoteTopic(topicId);
+	}
+}
